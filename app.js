3f8ccf5f739155f166cedae29c2417251a8c7fb4
Minerva <serrano_mine@hotmail.com>
2025-02-28 15:11:23 -0500

    inicio de js

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+prompt ('Me indicas el nombre de tus amigos por favor');
+
+let nombreDeAmigo = []
+
+if (nombreDeAmigo == '') {
+    alert ('Ingresa un dato valido');
+}
