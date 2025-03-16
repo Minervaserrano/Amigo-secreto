26c81cbc41ef3618ef1515c9507385914d956d69
Minerva <serrano_mine@hotmail.com>
2025-03-16 12:30:27 -0400

    Desarrollo de Js

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let nombre = [];
 
-// esta parte no tiene que ir: prompt ('Me indicas el nombre de tus amigos por favor');
+function agregarAmigo () {
+    let amigo = document.getElementById('amigo').value;
 
-let nombreDeAmigo = [];
+    if (amigo !=="") {
+        nombre.push(amigo);
 
-// despues del array usar: document.getElementById o document.querySelector para obtener el texto ingresado por el usuario
-let titulo = document.queryselector ('h1');
-titulo.innerHTML = 'Amigo Secreto';
+        document.getElementById('listaAmigos').innerHTML = "";
 
-let parrafo = document.queryselector ('p');
-parrafo.innerHTML = 'Digite el nombre de sus amigos';
+        mostrarAmigos();
 
-function agregarAmigo() {
-    console.log ('hola');
- }
-
-//faltaria aqui agregar un while
-while (nombreDeAmigo != '') {
-    console.log (nombreDeAmigo);
-
-    if (nombreDeAmigo == '') {
-        alert ('Ingresa un dato valido');
+        document.getElementById('amigo').value = "";
+    } else {
+        alert ("Por favor, inserte un nombre.");
     }
+}  
+    function mostrarAmigos(){
+        nombre.forEach(function(amigo){
+
+            let li = document.createElement('li');
+            li.textContent = amigo;
+        
+            document.getElementById('listaAmigos').appendChild(li);
+        });
+     
+    }
+
+function sortearAmigo (){
+    if (nombre.length > 0) {
+        
+        let indiceAleatorio = Math.floor(Math.random() * nombre.length);
+        let amigoSeleccionado = nombre[indiceAleatorio];
+        document.getElementById('resultado').textContent = 'Amigo aleatorio seleccionado: ' + amigoSeleccionado;
+
+    
+    } 
 }
-
-// Puedo usar tambien la frase sugerida: "Por favor, inserte un nombre"
-
-// Actualizar el array de amigos: Si el valor es válido, añadirlo al arreglo que almacena los nombre de amigos usando el método.push().
-// Limpiar el campo de entrada: Después de añadir el nombre, restablecer el campo de texto a una cadena vacía.
-
-// crear funcion para llamar la funcion de los botones
